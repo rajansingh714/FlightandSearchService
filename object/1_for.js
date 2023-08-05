734a432122563f1be1e9bf54e706b5321fa61f13
rajansingh70707@gmail.com <rajansingh70707@gmail.com>
2023-08-05 10:28:53 +0530

    Added flights create crud

---
+++ object/1_for.js
@@ -0,0 +1,12 @@
+let user= {
+    name: "rajan",
+    roll: 46,
+    from: "bihar"
+};
+
+
+for(let swag in user)
+{
+    console.log(user[swag]);
+    //console.log(swag);
+}
---
+++ src/services/flight-service.js
@@ -0,0 +1,46 @@
+const {AirplaneRepository,FlightRespository}= require('../respository/index');
+const {compareTime}= require('../utils/helper');
+class FlightService{
+
+    constructor(){
+        this.airplaneRepository= new AirplaneRepository();
+        this.flightrepository= new FlightRespository();
+    }
+
+
+    async createFlight(){
+        try{
+            if(!compareTime(data.arrivalTime, data.departureTime)){
+                throw{error: 'Arrival time cannot be less than departure time'};
+            }
+            const airplane= await this.airplaneRepository.getAirplane(data.airplaneId);
+            const flight= await this.flightrepository.createFlight({...data, totalSeats:airplane.capacity});
+            return flight;
+        }
+        catch(error){
+            console.log("something went woring at service layer");
+            throw{error};
+        }
+    }
+
+    async getFlightData(){
+
+    }
+
+
+}
+module.exports= FlightService;
+
+/**
+ * {
+ * flightnumber,
+ * airplaneId,
+ * departureAirportId,
+ * arrivalAirportId,
+ * arrivalTime,
+ * departureTime,
+ * price,
+ * totalSeats ->airplane
+ * 
+ * }
+ */
